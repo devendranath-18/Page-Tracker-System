7a3329d93cb2851d794b9b17698cfc688dc29da0
devendranath-18 <devendranathkapa@gmail.com>
2026-02-19 17:33:39 +0530

    Update visitRoutes.js

--- visitRoutes.js
+++ visitRoutes.js
@@ -1,12 +1,10 @@
-const express = require("express");
-const router = express.Router();
-
-const visitController = require("../controllers/visitController");
-
-
-router.post("/visits", visitController.trackVisit);
-
-
-router.get("/visits", visitController.getVisits);
+const router = require("express").Router();
+const {
+  trackVisit,
+  getAnalytics,
+} = require("../controllers/visitController");
+const authMiddleware = require("../middleware/authMiddleware");t
+router.post("/track", trackVisit);
+router.get("/", authMiddleware, getAnalytics);
 
 module.exports = router;
