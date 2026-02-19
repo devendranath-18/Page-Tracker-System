2ea438b13300de5cba7b9b7c6614a3c3017d7cfe
devendranath-18 <devendranathkapa@gmail.com>
2026-02-19 17:32:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-
-const sequelize = require("./config/db.js"); 
+const sequelize = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const visitRoutes = require("./routes/visitRoutes");
-
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
-app.use("/api", visitRoutes);
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.use("/api/auth", authRoutes);
+app.use("/api/visits", visitRoutes);
+const PORT = 5000; 
+sequelize.sync().then(() => {
+  console.log("DB Synced");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
