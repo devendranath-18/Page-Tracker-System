a1130daa42ae29ca82dfbb83af120250f34d711c
devendranath-18 <devendranathkapa@gmail.com>
2026-02-19 17:33:12 +0530

    Update visitController.js

--- visitController.js
+++ visitController.js
@@ -1,37 +1,36 @@
 const Visit = require("../models/visit");
-
 exports.trackVisit = async (req, res) => {
   try {
-    const ipAddress =
-      req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-
     const { page } = req.body;
-
+    const ipAddress =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress;
     let visit = await Visit.findOne({
       where: { ipAddress, page },
     });
-
     if (visit) {
       visit.count += 1;
       visit.lastVisitedAt = new Date();
       await visit.save();
     } else {
-      visit = await Visit.create({
+      await Visit.create({
         ipAddress,
         page,
         count: 1,
         lastVisitedAt: new Date(),
       });
     }
-
-    res.json(visit);
+    res.json({ message: "Visit tracked successfully" });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server Error" });
+    res.status(500).json({ error: error.message });
   }
 };
-
-exports.getVisits = async (req, res) => {
-  const visits = await Visit.findAll();
-  res.json(visits);
+exports.getAnalytics = async (req, res) => {
+  try {
+    const data = await Visit.findAll();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
